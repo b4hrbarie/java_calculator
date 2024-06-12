c7aa16fc9cfe24afad8d1314df8ab73e60843e14
Bárbara Moura <109366684+b4hrbarie@users.noreply.github.com>
2024-06-12 19:12:41 -0300

    V 1.0
    Calculadora básica com funções de soma, subtração, multiplicação e divisão (função de usar o resultado anterior em futuras operações).

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+public class Calculator {
+
+    private double number;
+    private double number2;
+    public double result = 0;
+
+    public void setNumber1(double number) {
+        this.number = number;
+    }
+
+    public void setNumber2(double number2) {
+        this.number2 = number2;
+    }
+
+    public void useRes() {
+        this.number = this.result;
+    }
+
+    public double Sum() {
+        return this.result = this.number + this.number2;
+    }
+
+    public double Sub() {
+        return this.result = this.number - this.number2;
+    }
+
+    public double Mult() {
+        return this.result = this.number * this.number2;
+    }
+
+    public double Div() {
+        return this.result = this.number / this.number2;
+    }
+}
---
+++ main.java
@@ -0,0 +1,75 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+
+public static void main(String[] args) {
+  Calculator myCalc = new Calculator();
+  char useRes = 'n';
+  char operator = ' ';
+  char continues = 's';
+  System.out.println("Boas vindas à calculadora!");
+  
+  while (continues == 's') {
+      switch (useRes){
+        case 'n':
+         System.out.println("Digite o primeiro número:");
+         Scanner scan1 = new Scanner(System.in);
+         double numb1 = scan1.nextDouble();
+         myCalc.setNumber1(numb1);
+         break;
+        case 's':
+          myCalc.useRes();
+          break;
+        default:
+          System.out.println("Opção inválida!");
+      }  
+        
+      System.out.println("Digite o segundo número:");
+      Scanner scan2 = new Scanner(System.in);
+      double numb2 = scan2.nextDouble();
+      myCalc.setNumber2 (numb2);
+      
+    System.out.println("Digite o operador:");
+    System.out.println("1 - Soma");
+    System.out.println("2 - Subtração");
+    System.out.println("3 - Multiplicação");
+    System.out.println("4 - Divisão");
+    Scanner scan3 = new Scanner(System.in);
+    operator = scan3.next().charAt(0);
+    
+     switch (operator){
+       case '1':
+         myCalc.result = myCalc.Sum();
+         System.out.println("O resultado da soma é: " + myCalc.result);
+         break;
+       case '2':
+         myCalc.result = myCalc.Sub();
+         System.out.println("O resultado da subtração é: " + myCalc.result);
+         break;
+       case '3':
+         myCalc.result = myCalc.Mult();
+         System.out.println("O resultado da multiplicação é: " + myCalc.result);
+         break;
+       case '4':
+         myCalc.result = myCalc.Div();
+         System.out.println("O resultado da divisão é: " + myCalc.result);
+         break;
+       default :
+        System.out.println("Operador inválido");  
+     }
+  
+  System.out.println("Deseja contiuar calculando? (s = sim / outro caractere = sair)");
+  Scanner scan4 = new Scanner(System.in);
+  continues = scan4.next().charAt(0);
+    if (continues =='s') {
+        System.out.println("Deseja usar o resultado do cálculo anterior na próxima operação?         (s = sim / n = não)");
+         Scanner scan5 = new Scanner(System.in);
+         useRes = scan5.next().charAt(0);
+    }
+}
+
+}
+  
+}
